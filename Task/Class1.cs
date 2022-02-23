1d5208893e4299f29c11b87fb778560ce6407a00
Your Name <bizi1298@gmail.com>
2022-02-23 19:57:25 +0500

    Добавьте файлы проекта.

---
+++ Task/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace Task
+{
+    public class MyStack
+    {
+        private int count; 
+        private Element last; 
+
+        public int Count => count; 
+        public bool IsEmpty => count == 0; 
+
+        public MyStack() 
+        {
+            count = 0;
+            last = null;
+        }
+
+        public void Push(ValueType value) 
+        {
+            count++;
+
+            last = new Element (last, value);
+        }
+
+        public ValueType Pop() 
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Stack Is Already Empty!");      
+            }
+
+            var value = last.Value;
+            count--;
+            last = last.Previous;
+
+            return value;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            last = null;
+        }
+    }
+
+    public class Element
+    {
+        public Element Previous { get; }
+        public ValueType Value { get; }
+
+        public Element (Element previous, ValueType value) 
+        {
+            Previous = previous;
+            Value = value;
+        }
+
+    }
+}
---
+++ TestTask/UnitTest1.cs
@@ -0,0 +1,120 @@
+using NUnit.Framework;
+using System;
+using Task;
+
+namespace TestTask
+{
+    [TestFixture]
+    public class Tests
+    {
+        MyStack stack;
+
+        [SetUp]
+        public void StackCreation()
+        {
+            stack = new MyStack();
+        }
+
+
+        [Test]
+        public void EmptyHasZeroCount()
+        {
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [Test]
+        public void CountWorksCorrectly()
+        {
+            stack.Push(1);
+            stack.Push(1);
+            stack.Push(1);
+            stack.Pop();
+            Assert.AreEqual(2, stack.Count);
+        }
+
+        [Test]
+        public void PushsAndPopsWorkCorrectly()
+        {
+            stack.Push('a');
+            stack.Push(1);
+            stack.Push(2.1);
+            stack.Pop();
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        [Test]
+        public void StackPopsToCleanCorrectly()
+        {
+            stack.Push(1);
+            stack.Pop();
+            CleanStackDontPops();
+        }
+
+        [Test]
+        public void CleanStackDontPops()
+        {
+            var exceptionCatched = false;
+            try
+            {
+                stack.Pop();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.AreEqual("Stack Is Already Empty!", ex.Message);
+                exceptionCatched = true;
+            }
+            catch(Exception)
+            {
+                Assert.Fail();
+                exceptionCatched = true;
+            }
+            if (exceptionCatched == false) Assert.Fail(); 
+        }
+
+        [Test]
+        public void IsEmptyWorksCorrectly1()
+        {
+            stack.Push(1);
+            stack.Pop();
+            Assert.AreEqual(true, stack.IsEmpty);
+        }
+
+        [Test]
+        public void IsEmptyWorksCorrectly2()
+        {
+            stack.Push(1);
+            Assert.AreEqual(false, stack.IsEmpty);
+        }
+
+        [Test]
+        public void ClearWorksCorrectly1()
+        {
+            stack.Push(1);
+            stack.Clear();
+            Assert.AreEqual(true, stack.IsEmpty);
+        }
+
+        [Test]
+        public void ClearWorksCorrectly2()
+        {
+            stack.Push(1);
+            stack.Clear();
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [Test]
+        public void ClearWorksCorrectly3()
+        {
+            stack.Push(1);
+            stack.Clear();
+            CleanStackDontPops();
+        }
+
+        [Test]
+        public void name()
+        {
+
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
